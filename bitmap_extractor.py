a955c767e995c38524dbbb946c1d8aa2df03937e
kazact <kazact95@gmail.com>
2026-09-02 18:05:03 -0500

    Create bitmap_extractor.py

---
+++ bitmap_extractor.py
@@ -0,0 +1,5 @@
+"""A standalone script for downscaling and pulling bitmaps from images.
+
+Decided to make this a standalone script. I'll probably just include the final bitmap data in the final project. My
+hope is to be able to reuse this in the future, similar to the basic font set and drawing class I'm using.
+"""
