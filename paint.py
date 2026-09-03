b88f765b9089dc4db11e4c89de967a875fbade4d
kazact <kazact95@gmail.com>
2026-09-03 16:49:31 -0500

    Initial commit.

---
+++ paint.py
@@ -0,0 +1,5 @@
+"""A standalone script for painting bitmaps in the terminal.
+
+Just about finished planning and realized this is probably best kept as a standalone script similar to extractor.
+I'm going to want to reuse this for future projects, but I'm not sure how I will go about including it.
+"""
