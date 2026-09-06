ba57dd54736fdfaa344a879bac5c16e4cf7109a8
kazact <kazact95@gmail.com>
2026-09-06 11:57:16 -0500

    Update TODO

--- tip_calc.py
+++ tip_calc.py
@@ -1,6 +1,7 @@
 """A tip calculator program.
 
 TODO:
+    * Sanitize inputs.
     * Start integrating logo. Will have to copy in parts of extractor and paint since I can't do imports.
     * Decide final project layout. Am I hardcoding bitmap or copying my extractor code into this module?
     * Allow tip option number to be used in place of a value.
