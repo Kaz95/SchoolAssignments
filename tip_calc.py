5eb1b73aa64e71b152110e323f24fc21fb5ef3fe
kazact <kazact95@gmail.com>
2026-09-06 14:53:47 -0500

    Add code from other modules.

--- tip_calc.py
+++ tip_calc.py
@@ -12,10 +12,15 @@ TODO:
 
 
 """
+import json
 import sys
-import bitmap_extractor
-import paint
+import time
+import urllib.request
 
+# import bitmap_extractor
+# import paint
+
+PANERA_LOGO_BITMAP_REMOTE = 'https://raw.githubusercontent.com/Kaz95/SchoolAssignments/refs/heads/master/panera_logo_bitmap.json'
 TERMINAL_WIDTH = 80
 """Hardcoded terminal width in characters."""
 
@@ -257,9 +262,94 @@ class TipCalc:
     def to_float(cls, value) -> str:
         return f'{int(value):04.2f}'
 
+# From other modules
+TERMINAL_BLACK = (12, 12, 12)
+PIXEL = '\u2584'
+
+# TODO: Figure out what I want to do about redundant global vars. Probably need to reuse them, but I'm lazy.
+# ANSI escapes
+RESET = "\x1b[0m"
+CURSOR_TO_TOP = "\x1b[H"  # Moves text cursor to 0,0 without clearing screen
+CLEAR_SCREEN = "\x1b[2J"  # Completely clears the terminal buffer once
+HIDE_CURSOR = "\x1b[?25l"  # Hides flashing text terminal bar
+SHOW_CURSOR = "\x1b[?25h"  # Restores terminal cursor state
+
+
+def load_bitmap(remote_bitmap):
+    with urllib.request.urlopen(remote_bitmap) as response:
+        panera_logo_bitmap = json.load(response)
+        return panera_logo_bitmap
+
+
+def lerp(starting_color, target_color, progress):
+    r = int(starting_color[0] + (target_color[0] - starting_color[0]) * progress)
+    g = int(starting_color[1] + (target_color[1] - starting_color[1]) * progress)
+    b = int(starting_color[2] + (target_color[2] - starting_color[2]) * progress)
+
+    return r, g, b
+
+def paint_a_frame(bit_map, progress, terminal_width, is_black=True, pad=False):
+
+    # Overwrite the previous frame by pinning cursor back to the top left corner
+    sys.stdout.write(CURSOR_TO_TOP)
+
+    height = len(bit_map)
+    width = len(bit_map[0])
+    if pad:
+        pad_length = (terminal_width - width) // 2
+        pad = ' ' * pad_length
+    else:
+        pad = ''
+    for y in range(0, height, 2):
+        line_buffer = [pad]
+        for x in range(width):
+            original_top = bit_map[y][x]
+            original_bottom = bit_map[y + 1][x]
+
+            if is_black:
+                top_rgb = lerp(TERMINAL_BLACK, original_top, progress)
+                bot_rgb = lerp(TERMINAL_BLACK, original_bottom, progress)
+
+            else:
+                top_rgb = lerp(original_top, TERMINAL_BLACK, progress)
+                bot_rgb = lerp(original_bottom, TERMINAL_BLACK, progress)
+
+            bg_ansi = f"\x1b[48;2;{top_rgb[0]};{top_rgb[1]};{top_rgb[2]}m"
+            fg_ansi = f"\x1b[38;2;{bot_rgb[0]};{bot_rgb[1]};{bot_rgb[2]}m"
+
+            line_buffer.append(f"{bg_ansi}{fg_ansi}{PIXEL}")
+
+        sys.stdout.write("".join(line_buffer) + RESET + "\n")
+    sys.stdout.flush()
+
+
+
+
+def play_animation_sequence(matrix, steps=60, sleep_rate=0.05):
+    # Hide interface cursors
+    sys.stdout.write(HIDE_CURSOR)
+    sys.stdout.write(CLEAR_SCREEN)
+
+    for step in range(steps + 1):
+        progress = step / steps
+        paint_a_frame(matrix, progress, TERMINAL_WIDTH)
+        time.sleep(sleep_rate)
+
+    for step in range(steps + 1):
+        progress = step / steps
+        paint_a_frame(matrix, progress, TERMINAL_WIDTH, is_black=False)
+        if step == 0:
+            time.sleep(3)
+        else:
+            time.sleep(sleep_rate)
+
+    sys.stdout.write(SHOW_CURSOR)
+    print("\nAnimation Complete.")
+
+
 if __name__ == '__main__':
-    logo_bitmap = bitmap_extractor.load_bitmap(bitmap_extractor.panera_logo_bitmap_remote)
-    paint.play_animation_sequence(logo_bitmap, 60, .05)
+    logo_bitmap = load_bitmap(PANERA_LOGO_BITMAP_REMOTE)
+    play_animation_sequence(logo_bitmap, 60, .05)
     # TipCalc.get_bill()
     # TipCalc.get_tip()
     # TipCalc.get_option()
